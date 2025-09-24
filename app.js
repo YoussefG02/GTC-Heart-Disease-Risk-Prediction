54d9d3a0bfd32af8e710aa3ad612d41b6c37270b
Youssef <youssefgamal02@gmail.com>
2025-09-24 21:11:30 +0300

    Updated fetch url

--- app.js
+++ app.js
@@ -42,7 +42,7 @@ document.addEventListener('DOMContentLoaded', function () {
                 data[key] = parseFloat(data[key]);
             });
 
-            const response = await fetch("http://127.0.0.1:8000/predict", {
+            const response = await fetch("https://heart-disease-risk-prediction.up.railway.app/predict", {
                 method: "POST",
                 headers: {
                     "Content-Type": "application/json"
